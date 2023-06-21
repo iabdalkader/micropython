0103f9412e3385c6e3e4bfa8161a20bfbbb69bda
iabdalkader <i.abdalkader@gmail.com>
2023-06-21 17:15:46 +0200

    extmod: Add ESP-Hosted networking interface.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- extmod/extmod.mk
+++ extmod/extmod.mk
@@ -38,6 +38,7 @@ SRC_EXTMOD_C += \
 	extmod/moduzlib.c \
 	extmod/modwebrepl.c \
 	extmod/network_cyw43.c \
+	extmod/network_esp_hosted.c \
 	extmod/network_lwip.c \
 	extmod/network_ninaw10.c \
 	extmod/network_wiznet5k.c \
